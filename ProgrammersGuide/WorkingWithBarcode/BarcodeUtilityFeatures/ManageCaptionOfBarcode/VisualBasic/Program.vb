f003bfd36538443d6ddd205c8f81a8de679749ee
iqbal-aspose <muhammad.iqbal@aspose.com>
2013-04-04 09:39:35 +0500

    Addition of Data directory validity checks

--- ProgrammersGuide/WorkingWithBarcode/BarcodeUtilityFeatures/ManageCaptionOfBarcode/VisualBasic/Program.vb
+++ ProgrammersGuide/WorkingWithBarcode/BarcodeUtilityFeatures/ManageCaptionOfBarcode/VisualBasic/Program.vb
@@ -15,6 +15,12 @@ Namespace ManageCaptionOfBarcode
 			' The path to the documents directory.
 			Dim dataDir As String = Path.GetFullPath("../../../Data/")
 
+			' Create directory if it is not already present.
+			Dim IsExists As Boolean = System.IO.Directory.Exists(dataDir)
+			If Not IsExists Then
+				System.IO.Directory.CreateDirectory(dataDir)
+			End If
+			
 			'Instantiate barcode object
 			Dim bb As New BarCodeBuilder()
 
