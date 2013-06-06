ed50dc2a16d92126e460d70cb3689b58ee5866a9
Fatima Ahmad <fatima.rabbiya@aspose.com>
2013-06-06 11:00:46 +0500

    Added New Examples
    Added New Examples

---
+++ ProgrammersGuide/WorkingWithBarcode/BarcodeAdvancedFeatures/SetStartAndStopSymbolsOfCodabarBarcode/CSharp/Program.cs
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////
+// Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.BarCode. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+using System.IO;
+
+using Aspose.BarCode;
+
+namespace SetStartAndStopSymbolsOfCodabarBarcode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The path to the documents directory.
+            string dataDir = Path.GetFullPath("../../../Data/");
+
+            // Create instance of BarCodeBuilder, specify codetext and symbology in the constructor
+            BarCodeBuilder builder = new BarCodeBuilder("11112222333344", Symbology.Codabar);
+
+            // Set the codabar start symbol to A
+            builder.CodabarStartSymbol = CodabarSymbol.A;
+
+            // Set the codabar stop symbol to D
+            builder.CodabarStopSymbol = CodabarSymbol.D;
+
+            // Save the image to disk in PNG format
+            builder.Save(dataDir + "barcode.png");
+
+        }
+    }
+}
---
+++ ProgrammersGuide/WorkingWithBarcode/BarcodeUtilityFeatures/SpecifySymbologiesForBarcodes/VisualBasic/Program.vb
@@ -0,0 +1,35 @@
+'////////////////////////////////////////////////////////////////////////
+' Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+'
+' This file is part of Aspose.BarCode. The source code in this file
+' is only intended as a supplement to the documentation, and is provided
+' "as is", without warranty of any kind, either expressed or implied.
+'////////////////////////////////////////////////////////////////////////
+
+Imports Microsoft.VisualBasic
+Imports System.IO
+
+Imports Aspose.BarCode
+
+Namespace SpecifySymbologiesForBarcodes
+	Public Class Program
+		Public Shared Sub Main(ByVal args() As String)
+			' The path to the documents directory.
+			Dim dataDir As String = Path.GetFullPath("../../../Data/")
+
+			'Instantiate BarCodeBuilder object
+			Dim bb As New BarCodeBuilder()
+
+			'Set the Code text for the barcode
+			bb.CodeText = "1234567"
+
+			'Set the symbology type to Code128
+			bb.SymbologyType = Symbology.Code128
+
+			'Save the image to your system and set its image format to Jpeg
+			bb.Save(dataDir & "barcode.jpg", System.Drawing.Imaging.ImageFormat.Jpeg)
+
+
+		End Sub
+	End Class
+End Namespace
